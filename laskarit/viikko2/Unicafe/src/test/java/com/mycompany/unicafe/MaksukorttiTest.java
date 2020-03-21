1fa7a8c7386d0e7e17c61a2f7c04705628779ee7
Pinja Vierimaa <pinja.vierimaa@helsinki.fi>
2020-03-21 17:34:26 +0200

    Added tests to Maksukortti class.

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,37 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void alussaSaldoOikein(){
+        assertEquals(10,kortti.saldo());
+    }
+    
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein(){
+        kortti.lataaRahaa(5);
+        assertEquals(15,kortti.saldo());
+    }
+    
+    @Test
+    public void saldoVaheneeOikein(){
+        kortti.otaRahaa(3);
+        assertEquals(7,kortti.saldo());
+    }
+    
+    @Test
+    public void saldoEiMuutuJosRahaaLiianVahan(){
+        kortti.otaRahaa(15);
+        assertEquals(10,kortti.saldo());
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaTrueJosOnnistuu(){
+        assertEquals(true, kortti.otaRahaa(5));
+    }
+    
+    @Test
+    public void otaRahaaPalauttaaFalseJosEpaonnistuu(){
+        assertEquals(false, kortti.otaRahaa(30));
+    }
 }
